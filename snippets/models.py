787e79eb39aa777e9587a5f131993346ee362b60
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-03 19:37:43 +0800

    修改错误代码

--- snippets/models.py
+++ snippets/models.py
@@ -9,7 +9,7 @@ from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICE = sorted((item, item) for item in get_all_styles())
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -17,7 +17,7 @@ class Snippet(models.Model):
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language =  models.CharField(choices=LANGUAGE_CHOICES,default='python',max_length=100)
-    style =  models.CharField(choices=STYLE_CHOICE,default='friendly',max_length=100)
+    style =  models.CharField(choices=STYLE_CHOICES,default='friendly',max_length=100)
 
     class Meta:
         ordering = ('created',)
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES,STYPLE_CHOICES
+from snippets.models import Snippet, LANGUAGE_CHOICES,STYLE_CHOICES
 
-class SnippetSerializer(serilizer.Serializer):
+class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False,allow_blank=True,max_length=100)
     code = serializers.CharField(style={'base_template':'textarea.html'})
     linenos = serializers.BooleanField(required=False)
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,default='python')
-    style = serializers.ChoiceField(choice=STYLE_CHOICE,default='friendly')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES,default='friendly')
 
     def create(self, validated_data):
         """Create and return a new `Snippet` instance,given the validated data
