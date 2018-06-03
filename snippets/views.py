e885a1c3a7f05d1f44da9a77fa3b887583114b10
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-03 20:39:18 +0800

    Writing regular Django views using our Serializer--Part One 1.我们只实现了`snippet_list`视图函数，及其对应的`urlsConf`。 2.使用`httpie`命令:获取`snippet列表`;`POST`一个`snippet`。 详见文档《guide-httpie.txt》

--- snippets/views.py
+++ snippets/views.py
@@ -4,3 +4,34 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets,many=True)
+        return JsonResponse(serializer.data,safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        else:
+            return JsonResponse(serializer.errors,status=400)
+
+def snippet_detail(request,pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    pass
