ec15b6810375a2cfc123dc1abbb293b9752615ab
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-03 21:33:23 +0800

    1.完成`snippet_detail`视图函数，实现了`获取`，`更新`，`删除`，某一个具体的`Snippet`。 2.完善了`guide-httpie.txt`;添加了`获取`，`更新`，`删除`等测试。

--- snippets/views.py
+++ snippets/views.py
@@ -30,8 +30,26 @@ def snippet_list(request):
         else:
             return JsonResponse(serializer.errors,status=400)
 
+@csrf_exempt
 def snippet_detail(request,pk):
     """
     Retrieve, update or delete a code snippet.
     """
-    pass
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse(serializer.errors,status=400)
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
