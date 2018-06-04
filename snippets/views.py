6cb2e05957bf2394270cf8ceba850ad2abae0aaf
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-04 10:33:26 +0800

    使用`通用`的`视图类` Using generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -7,33 +7,19 @@ from django.shortcuts import render
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
 
     """
     List all code snippets, or create a new snippet.
     列出所有已经存在的snippet或者创建一个新的snippet
     """
-
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request,*args,**kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request,*args,**kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     """
     Retrieve, update or delete a code snippet.
@@ -41,12 +27,3 @@ class SnippetDetail(mixins.RetrieveModelMixin,
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request,args,kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request,args,kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request,args,kwargs)
