44bae3976530e9c596e7f4276cca69edd51de26b
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-04 22:37:34 +0800

    添加Schema

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from snippets import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -10,6 +12,9 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 
+schema_view = get_schema_view(title='Pastebin API')
+
 urlpatterns = [
-     url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
+    url(r'^', include(router.urls)),
 ]
