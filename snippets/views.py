6f4a96b7e18667bf7b5042006b7452c88f641e94
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-04 20:35:28 +0800

    1.为API创建根URL 在`根URL`，可以看到，分布指向`Users`和`Snippet`的`List列表`的`URL链接`。 2.创建跳转至查看高亮代码段的URL 创建一个视图类`SnippetHighlight`，专门负责把某一个`snippet`的`highlighted`，渲染为HTML格式。

--- snippets/views.py
+++ snippets/views.py
@@ -8,11 +8,20 @@ from django.shortcuts import render
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer,UserSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 
-
+@api_view(['GET'])
+def api_root(request,format=None):
+    return Response({
+        'users': reverse('user-list',request=request,format=format),
+        'snippets': reverse('snippet-list',request=request,format=format)
+    })
 
 class SnippetList(generics.ListCreateAPIView):
 
@@ -38,6 +47,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
 
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self,request,*args,**kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
 
