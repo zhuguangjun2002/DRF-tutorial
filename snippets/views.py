07cc8104242ec8ea0b7b5baf2a67b572da6004b1
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-04 22:21:43 +0800

    使用Routers:神奇！

--- snippets/views.py
+++ snippets/views.py
@@ -7,23 +7,13 @@ from django.shortcuts import render
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer,UserSerializer
-from rest_framework import generics
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework.decorators import detail_route
 from snippets.permissions import IsOwnerOrReadOnly
-
-@api_view(['GET'])
-def api_root(request,format=None):
-    return Response({
-        'users': reverse('user-list',request=request,format=format),
-        'snippets': reverse('snippet-list',request=request,format=format)
-    })
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
