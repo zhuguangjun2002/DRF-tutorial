c6a26b7a2851ece0ef7ce64aab2e889348f565d4
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-04 17:46:50 +0800

    1.把Snippets和Users关联起来 通过`SnipList`视图类中，添加方法`perform_create`,实现`Snip`的`owner`的`保存`。 2.更新serializer 更新`SnippetSerializer`，添加字段`owner`，使其序列化时，显示`owner`的`username`。 3.添加权限 为`SnippetList`视图类，`SnippetDetail`视图类，添加权限：`IsAuthenticatedOrReadOnly`。 `已认证或者只读`权限：要么`方法`是只读的，可以访问。要么`已认证`，可以访问。 针对`SnippetList`视图类,就是，`GET`方法，大家都可访问；`POST`方法，只有`已认证`用户，才可以访问。 针对`SnipnetDetail`视图类，`GET`方法，大家都可以方法；`PUT`方法，`DELETE`方法，只有`已认证`用户，才可以访问。

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,14 +4,19 @@ from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    # 这里可以使用也 CharField(read_only=True) 来替换
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code',
+                  'linenos', 'language', 'style', 'owner')
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
     class Meta:
         model = User
         fields = ('id', 'username', 'snippets')
--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,8 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer,UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
+
 
 class SnippetList(generics.ListCreateAPIView):
 
@@ -18,6 +20,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
@@ -27,6 +33,8 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
 
 class UserList(generics.ListAPIView):
 
